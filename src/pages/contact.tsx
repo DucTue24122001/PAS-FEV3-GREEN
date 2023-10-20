03fa9c58409b797974d08071c56cb94e903f48cc
nhhabc <103050933+nhhabc@users.noreply.github.com>
2023-10-20 19:47:22 +0700

    fix some think

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,6 +2,7 @@ import { Flex, Image, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import fb_icon from "../../public/images/fb-icon.png";
 import tele_icon from "../../public/images/telegram-icon.png";
+import line_icon from "../../public/images/line_logo.png";
 import insta_icon from "../../public/images/insta_logo.webp";
 import mess_icon from "../../public/images/mess_logo.png";
 import mail_icon from "../../public/images/mail_logo.png";
@@ -20,7 +21,7 @@ const Contact = () => {
       case "telegram":
         return tele_icon.src
       case "line":
-        return 
+        return line_icon.src
       case "viber":
         return viber_icon.src
       case "facebook":
@@ -42,7 +43,7 @@ const Contact = () => {
     <Flex bg={"linear-gradient(90deg, #000a83 , #0011e6, #3544ff, #000a83 , #0011e6, #3544ff, #000a83 , #0011e6, #3544ff );"} m={["10px","10px","10px","0px"]}
       mb={"80px"}>
     <Flex bg={"#000435"} padding={"1px"} w={"100%"} margin={"1px 1px"} flexDir={"column"} border={"1px solid #0a502b"}>
-      {socialListInfo.map((item, i) => (
+      {socialListInfo.concat(contactListInfo).map((item, i) => (
         <Link key={i} padding={"20px"} href={item.link}>
           <Flex>
             <Image w={"60px"} h={"52px"} src={detectIco(item.name)} alt={item.content} objectFit={"contain"} />
